39be93e5a2977fb43e45ff39ec6c43e3a9755fec
ibrahim amoo <ibrahim_amoo@yahoo.com>
2020-09-30 20:05:09 +0100

    about page completed

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,8 +3,30 @@ import "./About.scss";
 import aboutImg from "../../assets/about/about-image.jpg";
 import ProcessCard from "../../components/processCard/ProcessCard";
 import TooltipRect from "../../components/toolTipRect/TooltipRect";
+import people1 from "../../assets/about/people1.png";
+import people2 from "../../assets/about/people2.png";
+import people3 from "../../assets/about/people3.png";
+import people4 from "../../assets/about/people4.png";
+import people5 from "../../assets/about/people5.png";
 
 function About() {
+  const peopleList = [
+    { pic: people1, name: "Ibrahim Amoo", role: "Front-end Developer" },
+    { pic: people2, name: "Uchenna Joel", role: "Back-end Developer" },
+    { pic: people3, name: "Tayo Osodein", role: "Legal Advisor" },
+    { pic: people4, name: "Kingsley Momah", role: "Web Designer" },
+    { pic: people5, name: "Joyce Olayinka", role: "Content Creator" },
+  ];
+  const processContent = () => {
+    return (
+      <ul>
+        <li>Frontend development</li>
+        <li>Frontend integration</li>
+        <li>Backend development</li>
+        <li>Quality Assurance</li>
+      </ul>
+    );
+  };
   return (
     <div className="about-container">
       <div
@@ -45,18 +67,37 @@ function About() {
       <section className="how-it-works">
         <h4 className="header">How It Works</h4>
         <div className="process-grid">
-          {[1, 2, 3].map((i) => (
+          {[1, 2, 3, 4].map((i) => (
             <ProcessCard
               key={i}
-              content="No content yet"
+              content={processContent()}
               header="Process Control"
             />
           ))}
         </div>
       </section>
       <section className="the-team">
-        {/* <h4 className="header">Meet The Team</h4> */}
         <TooltipRect content="Meet The Team" />
+        <div className="team-members">
+          {peopleList.map((item, i) => (
+            <div key={i} className="member-container">
+              <img src={item.pic} className="member-image" alt="people" />
+              <div className="member-profile">
+                <h2 className="content name">{item.name}</h2>
+                <p className="content role">{item.role}</p>
+                <p className="content">
+                  Aspiring Innovative Electronic and Electrical Engineer:
+                  Interested in control engineering and automation. Enjoy using
+                  technology to improve others’ lives and seeking to join the
+                  engineering world with a strong, dedicated mind.
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="footer">
+        © 2020 Copyright | All rights reserved
       </section>
     </div>
   );
